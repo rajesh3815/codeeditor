2a33056679a217b2fefb1e744f531a52844a2ca7
Rajesh Kumar <rkraja730@gmail.com>
2024-07-01 21:11:25 +0530

    site name chaged

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -48,7 +48,7 @@ const Editory = () => {
           }}
         />
       </div>
-      <p>made by Rajesh❣️ <a href="https://github.com/rajesh3815/codeeditor.git" target="_blank" rel="noopener noreferrer">github</a></p>
+      <p>made by Rajesh❣️ <a href="https://github.com/rajesh3815/codeeditor.git" target="_blank" rel="noopener noreferrer">GitHub</a></p>
     </div>
   );
 };
